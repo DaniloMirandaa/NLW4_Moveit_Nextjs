f8a748faf345d3e509902a31429d09ba0a060194
DaniloMirandaa <danilomirandaa.15@outlook.com>
2021-02-24 23:57:50 -0300

    commit_02_24_02_06

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,8 +4,9 @@ import styles from '../styles/components/CountDown.module.css';
 let countdownTimeout: NojeJS.Timeout;
 
 export function CountDown(){
-  const [time, setTime] = useState(25 * 60);
+  const [time, setTime] = useState(0.1 * 60);
   const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -28,6 +29,9 @@ export function CountDown(){
       countdownTimeout = setTimeout(()=>{
       setTime(time - 1);
       },1000)
+    }else if(isActive && time === 0){
+      setHasFinished(true);
+      setIsActive(false);
     }
   }, [isActive, time])
 
@@ -45,11 +49,19 @@ export function CountDown(){
         <span>{secondRight}</span>
       </div>
     </div>
-
-    {isActive ?(
+    {hasFinished ? (
+      <button 
+      type="button" 
+      className={styles.countdownButton}
+      >
+      Ciclo encerrado
+    </button>
+    ) : (
+      <>
+        {isActive ?(
       <button 
         type="button" 
-        className={'${styles.countdownButton}  ${styles.countdownButtonActive}'}
+        className={`${styles.countdownButton}  ${styles.countdownButtonActive}`}
         onClick={resetCountdown}
         >
         Abandonar ciclo
@@ -63,6 +75,10 @@ export function CountDown(){
         Iniciar um ciclo
       </button>
     ) }
+      </>
+    )}
+
+    
   </div>
   );
 }
